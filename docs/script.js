3fc2e8250971a043a5b84018995194e6bc25f47a
norimichi <opabinian525@gmail.com>
2026-04-19 17:25:50 +0900

    配置をjsに修正

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,39 @@
-let counts = {
-  count1: 0,
-  count2: 0
-};
+let counts = {};
+let initialValues = {};
+
+// ===== 左右共通のブロック生成 =====
+function createCounter(id, label, initial, step) {
+  counts[id] = initial;
+  initialValues[id] = initial; 
+
+  const box = document.createElement("div");
+
+  box.innerHTML = `
+    <button class="btn" onclick="add('${id}', ${step})">＋${step}</button>
+    <div class="count">${label}</div>
+    <div class="count" id="${id}">${initial}</div>
+    <button class="btn" onclick="sub('${id}', ${step})">ー${step}</button>
+  `;
+
+  return box;
+}
+
+// ===== 左右に配置 =====
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+left.appendChild(createCounter("count_lt_2", "〇", 0, 1));
+left.appendChild(createCounter("count_lt_1", "HP", 180, 10));
+
+right.appendChild(createCounter("count_rt_1", "HP", 180, 10));
+right.appendChild(createCounter("count_rt_2", "〇", 0, 1));
+
+
+
+
+// let counts = {
+//   count1: 0,
+//   count2: 0
+// };
 
 function update(id) {
   document.getElementById(id).innerText = counts[id];
@@ -18,10 +50,10 @@ function sub(id, num) {
 }
 
 function reset() {
-  counts.count1 = 0;
-  counts.count2 = 0;
-  update("count1");
-  update("count2");
+  for (let id in counts) {
+    counts[id] = initialValues[id];
+    update(id);
+  }
 }
 
 function dice() {
